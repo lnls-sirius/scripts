4e3de0999a4d42b743bbae183983241d3343c999
ximenes <xresende@gmail.com>
2018-11-13 17:04:56 -0200

    Update scripts

--- iocs/sirius-script-ioc-ps.bash
+++ iocs/sirius-script-ioc-ps.bash
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+# --- global variables ---
 fac_passwd=
+bbbs = (
+  "bbb-tb-correctors"
+  "bbb-tb-quadrupoles"
+  "bbb-tb-dipoles"
+  "bbb-as-dclinks")
+
+# --- aux functions ---
 
 function ioc_cmd {
   bbb=$1
@@ -8,7 +16,6 @@ function ioc_cmd {
   printf "=== $bbb : $cmd ===\n"
   sshpass -p $fac_passwd ssh fac@"$bbb" "sudo systemctl $cmd sirius-bbb-ioc-ps.service"
 }
-
 
 function get_password {
   read -s -r -p "fac user's password @ bbbs: " fac_passwd; echo ""
@@ -35,24 +42,29 @@ function start {
 function status {
   printf "Status of all power supply IOCs...\n"
   get_password
-  bbb="bbb-tb-quadrupoles" && ioc_cmd $bbb status; printf "\n\n"
-  bbb="bbb-tb-correctors" && ioc_cmd $bbb status; printf "\n\n"
-  bbb="bbb-tb-dipoles" && ioc_cmd $bbb status; printf "\n\n"
-  bbb="bbb-as-dclinks" && ioc_cmd $bbb status; printf "\n\n"
+  for bbb in "${bbbs[@]}"; do
+    ioc_cmd $bbb status; printf "\n\n"
+  fi
 }
 
 function print_help {
   printf "help not implemented!\n"
 }
 
-if [ -z "$1" ]; then
-  print_help
-elif [ "$1" == "stop" ]; then
-  stop
-elif [ "$1" == "start" ]; then
-  start
-elif [ "$1" == "status" ]; then
-  status
-else
-  print_help
-fi
+function run {
+  if [ -z "$1" ]; then
+    print_help
+  elif [ "$1" == "stop" ]; then
+    stop
+  elif [ "$1" == "start" ]; then
+    start
+  elif [ "$1" == "status" ]; then
+    status
+  else
+    print_help
+  fi
+}
+
+# --- run scripts ---
+
+run "$@"
