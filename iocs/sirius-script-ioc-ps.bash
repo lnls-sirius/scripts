430935050377f3f0dfed3cc57730fd219653f64a
linac <fernandohds564@gmail.com>
2018-11-13 16:59:49 -0200

    Update scripts

--- iocs/sirius-script-ioc-ps.bash
+++ iocs/sirius-script-ioc-ps.bash
@@ -4,24 +4,23 @@ fac_passwd=
 
 function ioc_cmd {
   bbb=$1
-  fac_passwd=$2
-  cmd=$3
+  cmd=$2
   printf "=== $bbb : $cmd ===\n"
   sshpass -p $fac_passwd ssh fac@"$bbb" "sudo systemctl $cmd sirius-bbb-ioc-ps.service"
 }
 
 
 function get_password {
-  read -s -r -p "fac user's password @ bbbs: " $fac_passwd; echo ""
+  read -s -r -p "fac user's password @ bbbs: " fac_passwd; echo ""
 }
 
 function stop {
   printf "Stopping all power supply IOCs...\n"
   get_password
-  bbb="bbb-tb-correctors" && ioc_cmd $bbb $fac_passwd stop; printf "\n\n"
-  bbb="bbb-tb-quadrupoles" && ioc_cmd $bbb $fac_passwd stop; printf "\n\n"
-  bbb="bbb-tb-dipoles" && ioc_cmd $bbb $fac_passwd stop; printf "\n\n"
-  bbb="bbb-as-dclinks" && ioc_cmd $bbb $fac_passwd stop; printf "\n\n"
+  bbb="bbb-tb-correctors" && ioc_cmd $bbb stop; printf "\n\n"
+  bbb="bbb-tb-quadrupoles" && ioc_cmd $bbb stop; printf "\n\n"
+  bbb="bbb-tb-dipoles" && ioc_cmd $bbb stop; printf "\n\n"
+  bbb="bbb-as-dclinks" && ioc_cmd $bbb stop; printf "\n\n"
 }
 
 function start {
@@ -36,10 +35,10 @@ function start {
 function status {
   printf "Status of all power supply IOCs...\n"
   get_password
-  bbb="bbb-tb-correctors" && ioc_cmd $bbb $fac_passwd status; printf "\n\n"
-  bbb="bbb-tb-quadrupoles" && ioc_cmd $bbb $fac_passwd status; printf "\n\n"
-  bbb="bbb-tb-dipoles" && ioc_cmd $bbb $fac_passwd status; printf "\n\n"
-  bbb="bbb-as-dclinks" && ioc_cmd $bbb $fac_passwd status; printf "\n\n"
+  bbb="bbb-tb-quadrupoles" && ioc_cmd $bbb status; printf "\n\n"
+  bbb="bbb-tb-correctors" && ioc_cmd $bbb status; printf "\n\n"
+  bbb="bbb-tb-dipoles" && ioc_cmd $bbb status; printf "\n\n"
+  bbb="bbb-as-dclinks" && ioc_cmd $bbb status; printf "\n\n"
 }
 
 function print_help {
