7d4345a7f520b2063c14378af408f82025b49ab4
ximenes <xresende@gmail.com>
2019-02-24 02:16:09 -0300

    Update scripts

--- bin/sirius-script-repos-update-install.sh
+++ bin/sirius-script-repos-update-install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-cp /home/sirius/repos/scripts/bin/sirius-script-repos-install.sh /usr/local/bin/
+tmpdir=$(mktemp /tmp/deploy-update-instal-XXXXXX)
+
+timestamp=`date '+%Y-%m-%d_%Hh%Mm%Ss'`
+tmpdir="/tmp/deploy-update-install-"$timestamp
+mkdir $tmpdir
+
+cd $tmpdir
+git clone https://github.com/lnls-sirius/scripts.git
+cp ./scripts/bin/sirius-script-repos-install.sh /usr/local/bin/
 chmod 755 /usr/local/bin/sirius-script-repos-install.sh
