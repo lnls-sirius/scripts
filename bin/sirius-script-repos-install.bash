d8f0544feacc380aea14c84bc48c12f6a710d0db
ximenes <xresende@gmail.com>
2019-03-04 13:18:39 -0300

    Update script files

--- bin/sirius-script-repos-install.bash
+++ bin/sirius-script-repos-install.bash
@@ -8,10 +8,18 @@ source ./sirius-script-bbb-utils.bash
 trap _abort SIGINT;
 
 
-function cmd_repo_install {
-  repo=$1
+function check_repo_install {
+  repo="$1"
   tmpdir=$2
-  cp -ra $mirror_repos_path/$repo $tmpdir/
+  fname="$tmpdir/log-install-$repo.stderr"
+  err=$(cat $fname)
+  echo $err
+}
+
+function cmd_repo_install {
+  repo="$1"
+  tmpdir="$2"
+  cp -a $mirror_repos_path/$repo $tmpdir/
   cd $tmpdir/$repo
   if [ "$repo" == "scripts" ]; then
     make install 1> ../log-install-$repo.stdout 2> ../log-install-$repo.stderr
@@ -31,16 +39,30 @@ function cmd_repo_install {
   else
     echo "installation not defined for $repo !" 1> log-install-$repo.stdout 2> log-install-$repo.stderr
   fi
+  var_error=$(check_repo_install $repo $tmpdir)
 }
+
+
+function install_all {
+  tmpdir=$1
+  mkdir -p $tmpdir
+  for repo in ${repos[@]}; do
+    printf_white "installing $repo ... "
+    cmd_repo_install $repo $tmpdir
+    if [[ -z "$var_error" ]]; then
+      printf_green "ok\n"
+    else
+      printf_red "fail\n"
+      exit
+    fi
+  done
+}
+
 
 
 function run {
   tmpdir=$(get_tmpdir repos-install)
-  echo $tmpdir
-  for repo in ${repos[@]}; do
-    cmd_repo_install $repo $tmpdir
-    echo $repo
-  done
+  install_all $tmpdir
 }
 
 
