44400d29740affe07e3cdda55ea2113dddc67f1d
ximenes <xresende@gmail.com>
2019-03-21 10:00:36 -0300

    Add help arg to sirius-script-services-lnls452.bash

--- bin/sirius-script-services-lnls452.bash
+++ bin/sirius-script-services-lnls452.bash
@@ -4,6 +4,30 @@ source /usr/local/bin/sirius-script-utils.bash
 trap _abort SIGINT;
 
 
+function print_help {
+    printf "NAME\n"
+    printf "       sirius-script-services-lnls452.bash - Interact with services in lnls452-linux\n"
+    printf "\n"
+    printf "SINOPSIS\n"
+    printf "       sirius-script-services-lnls452.bash [--help] CMD...\n"
+    printf "\n"
+    printf "DESCRIPTION\n"
+    printf "       Script used to interact with services running in lnls452-linux\n"
+    printf "\n"
+    printf "       --help               print this help\n"
+    printf "\n"
+    printf "       status               return status of services\n"
+    printf "\n"
+    printf "       start                start services\n"
+    printf "\n"
+    printf "       stop                 stop services\n"
+    printf "\n"
+    printf "       restart              stop services\n"
+    printf "\n"
+    printf "AUTHOR\n"
+    printf "       FACS-LNLS.\n"
+}
+
 function run_systemctl {
   cmd=$1
   printf_blue "Runnnig systemctl $cmd for services.\n"
@@ -15,11 +39,22 @@ function run_systemctl {
   done
 }
 
+function run {
+  if [ ! $# -eq 1 ]; then
+    print_help
+    exit
+  elif [[ "$1" == "--help" ]]; then
+    print_help
+    exit
+  fi
+  host=$(hostname)
+  if [[ "$host" == "lnls452-linux" ]]; then
+    run_systemctl $1
+  else
+    get_password sirius lnls452-linux
+    sshpass -p $user_passwd ssh -t sirius@lnls452-linux "sudo sirius-script-services-lnls452.bash $1"
+  fi
+}
 
-host=$(hostname)
-if [[ "$host" == "lnls452-linux" ]]; then
-  run_systemctl $1
-else
-  get_password sirius lnls452-linux
-  sshpass -p $user_passwd ssh -t sirius@lnls452-linux "sudo sirius-script-services-lnls452.bash $1"
-fi
+
+run $@
