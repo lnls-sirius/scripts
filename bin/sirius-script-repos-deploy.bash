81122d85abadebf3fbcd7b3359a708db787dceba
ximenes <xresende@gmail.com>
2019-04-04 12:36:56 -0300

    Chnage sirius-script-repos-deploy.bash
    - Delete temporary files in NFS server repos in order to aboid unnecessary rsync synchronization from BBB

--- bin/sirius-script-repos-deploy.bash
+++ bin/sirius-script-repos-deploy.bash
@@ -41,6 +41,7 @@ function checkout_tagged_repos_nfs_server {
     reponame=$(echo $repo | cut -d":" -f1)
     branch=$(echo $repo | cut -d":" -f2)
     printf_yellow "[$repo]\n"
+    sshpass -p $user_passwd ssh sirius@$servnfs_hostname "cd $servnfs_repos_folder/$reponame; rm -rf dist build */*.egg-info *.egg-info"
     sshpass -p $user_passwd ssh sirius@$servnfs_hostname "cd $servnfs_repos_folder/$reponame; git stash; git fetch --prune origin '+refs/tags/*:refs/tags/*'; git checkout $branch; git pull; git checkout $deploy_tag"
     printf "\n"
   done
