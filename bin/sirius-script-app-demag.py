c0250f27e26ad6be62d349d1345e6d71b13c539e
ximenes <xresende@gmail.com>
2019-05-07 11:46:07 -0300

    Improve demagnetization script
    - Change how waveforms are defined, with more intuitive parameters

--- bin/sirius-script-app-demag.py
+++ bin/sirius-script-app-demag.py
@@ -10,160 +10,167 @@ from multiprocessing import Process
 
 
 # obs:
-# it is preferable that nrpts_period = 4*(n+1), with n=1,2,...
+# In order for the waveform to end at zero current, as it is desirable,
+# please choose period as multiple of time step dt;
 
-_dt = 0.5  # [s]
 parms = {
-    # dt, ampl, nrpts_period, nr_periods, tau_period, sin_squared?
-    # 'TB-Fam:PS-B': [_dt, 250.0, 4*(18+1), 6, 1.5, True],  # 42.6 [A/s]
-    'TB-Fam:PS-B': [_dt, 300.0, 4*(3+1), 32, 4.0, True],  # 42.6 [A/s]
+    # psname           dt[s], max_amp[A], period[s], nr_cycles, tau[s], sin**2
 
-    'TB-01:PS-QD1': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-01:PS-QF1': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-02:PS-QD2A': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-02:PS-QF2A': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-02:PS-QD2B': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-02:PS-QF2B': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-03:PS-QD3': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-03:PS-QF3': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-04:PS-QD4': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-04:PS-QF4': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-01:PS-CH-1': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-01:PS-CV-1': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-01:PS-CH-2': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-01:PS-CV-2': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-02:PS-CH-1': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-02:PS-CV-1': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-02:PS-CH-2': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-02:PS-CV-2': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-04:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-04:PS-CV-1': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'TB-04:PS-CV-2': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
+    # --- TB ---
 
-    'BO-Fam:PS-B-1': [_dt, 60.0, 4*(22+1), 6, 1.5, False],  # 24 [A/s]
-    'BO-Fam:PS-B-2': [_dt, 60.0, 4*(22+1), 6, 1.5, False],  # 24 [A/s]
-    'BO-Fam:PS-QF': [_dt, 120.0, 4*(11+1), 10, 2, False],  # 34.8 [A/s]
-    'BO-Fam:PS-QD': [_dt, 30.0, 4*(11+1), 10, 2, False],  # 8.8 [A/s]
-    'BO-Fam:PS-SF': [_dt, 150.0, 4*(11+1), 10, 2, False],  # 43.5 [A/s]
-    'BO-Fam:PS-SD': [_dt, 150.0, 4*(11+1), 10, 2, False],  # 43.5 [A/s]
-    'BO-02D:PS-QS': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
+    'TB-Fam:PS-B':     [0.5, 300, 4, 32, 16, True],
 
-    'BO-01U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-01U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-03U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-03U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-05U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-05U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-07U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-07U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-09U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-09U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-11U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-11U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-13U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-13U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-15U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-15U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-17U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-17U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-19U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-19U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-21U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-21U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-23U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-23U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-25U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-25U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-27U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-27U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-29U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-29U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-31U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-31U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-33U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-33U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-35U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-35U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-37U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-37U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-39U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-39U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-41U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-41U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-43U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-43U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-45U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-45U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-47U:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-47U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-49D:PS-CH': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
-    'BO-49U:PS-CV': [_dt, 10.0, 4*(11+1), 10, 2, False],  # 2.9 [A/s]
+    'TB-01:PS-QD1':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-01:PS-QF1':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-02:PS-QD2A':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-02:PS-QF2A':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-02:PS-QD2B':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-02:PS-QF2B':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-03:PS-QD3':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-03:PS-QF3':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-04:PS-QD4':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-04:PS-QF4':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-01:PS-CH-1':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-01:PS-CV-1':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-01:PS-CH-2':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-01:PS-CV-2':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-02:PS-CH-1':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-02:PS-CV-1':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-02:PS-CH-2':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-02:PS-CV-2':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-04:PS-CH':     [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-04:PS-CV-1':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'TB-04:PS-CV-2':   [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
 
-    'LA-CN:H1DPPS-1': [_dt, 18.0, 4*(11+1), 10, 2, False],
+    # --- LI ---
 
-    'LA-CN:H1DQPS-1': [_dt, 5.0, 4*(11+1), 10, 2, False],
-    'LA-CN:H1DQPS-2': [_dt, 5.0, 4*(11+1), 10, 2, False],
-    'LA-CN:H1FQPS-1': [_dt, 5.0, 4*(11+1), 10, 2, False],
-    'LA-CN:H1FQPS-2': [_dt, 5.0, 4*(11+1), 10, 2, False],
-    'LA-CN:H1FQPS-3': [_dt, 5.0, 4*(11+1), 10, 2, False],
+    'LA-CN:H1DPPS-1':  [0.5, 18.0, 24, 10, 48, False],
 
-    'LA-CN:H1LCPS-1': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-2': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-3': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-4': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-5': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-6': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-7': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-8': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-9': [_dt, 1.5, 4*(11+1), 10, 2, False],
-    'LA-CN:H1LCPS-10': [_dt, 1.5, 4*(11+1), 10, 2, False],
+    'LA-CN:H1DQPS-1':  [0.5, 5, 24, 10, 48, False],
+    'LA-CN:H1DQPS-2':  [0.5, 5, 24, 10, 48, False],
+    'LA-CN:H1FQPS-1':  [0.5, 5, 24, 10, 48, False],
+    'LA-CN:H1FQPS-2':  [0.5, 5, 24, 10, 48, False],
+    'LA-CN:H1FQPS-3':  [0.5, 5, 24, 10, 48, False],
 
-    'LA-CN:H1MLPS-1': [_dt, 5.0, 4*(11+1), 10, 2, False],
-    'LA-CN:H1MLPS-2': [_dt, 5.0, 4*(11+1), 10, 2, False],
-    'LA-CN:H1MLPS-3': [_dt, 5.0, 4*(11+1), 10, 2, False],
-    'LA-CN:H1MLPS-4': [_dt, 5.0, 4*(11+1), 10, 2, False],
+    'LA-CN:H1LCPS-1':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-2':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-3':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-4':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-5':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-6':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-7':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-8':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-9':  [0.5, 1.5, 24, 10, 48, False],
+    'LA-CN:H1LCPS-10': [0.5, 1.5, 24, 10, 48, False],
 
-    'LA-CN:H1RCPS-1': [_dt, 15.0, 4*(11+1), 10, 2, False],
+    'LA-CN:H1MLPS-1':  [0.5, 5, 24, 10, 48, False],
+    'LA-CN:H1MLPS-2':  [0.5, 5, 24, 10, 48, False],
+    'LA-CN:H1MLPS-3':  [0.5, 5, 24, 10, 48, False],
+    'LA-CN:H1MLPS-4':  [0.5, 5, 24, 10, 48, False],
 
-    'LA-CN:H1SCPS-1': [_dt, 0.25, 4*(11+1), 10, 2, False],
-    'LA-CN:H1SCPS-2': [_dt, 0.25, 4*(11+1), 10, 2, False],
-    'LA-CN:H1SCPS-3': [_dt, 0.25, 4*(11+1), 10, 2, False],
-    'LA-CN:H1SCPS-4': [_dt, 0.25, 4*(11+1), 10, 2, False],
+    'LA-CN:H1RCPS-1':  [0.5, 15, 24, 10, 48, False],
 
-    'LA-CN:H1SLPS-1': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-2': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-3': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-4': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-5': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-6': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-7': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-8': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-9': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-10': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-11': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-12': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-13': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-14': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-15': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-16': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-17': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-18': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-19': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-20': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
-    'LA-CN:H1SLPS-21': [_dt, 35.0, 4*(18+1), 6, 1.5, True],
+    'LA-CN:H1SCPS-1':  [0.5, 0.25, 24, 10, 48, False],
+    'LA-CN:H1SCPS-2':  [0.5, 0.25, 24, 10, 48, False],
+    'LA-CN:H1SCPS-3':  [0.5, 0.25, 24, 10, 48, False],
+    'LA-CN:H1SCPS-4':  [0.5, 0.25, 24, 10, 48, False],
+
+    'LA-CN:H1SLPS-1':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-2':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-3':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-4':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-5':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-6':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-7':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-8':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-9':  [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-10': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-11': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-12': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-13': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-14': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-15': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-16': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-17': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-18': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-19': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-20': [0.5, 35, 38, 6, 57, True],
+    'LA-CN:H1SLPS-21': [0.5, 35, 38, 6, 57, True],
+
+    # --- BO ---
+
+    'BO-Fam:PS-B-1':   [0.5, 60,  46, 6,  69, False],  # 24 [A/s]
+    'BO-Fam:PS-B-2':   [0.5, 60,  46, 6,  69, False],  # 24 [A/s]
+    'BO-Fam:PS-QF':    [0.5, 120, 24, 10, 48, False],  # 34.8 [A/s]
+    'BO-Fam:PS-QD':    [0.5, 30,  24, 10, 48, False],  # 8.8 [A/s]
+    'BO-Fam:PS-SF':    [0.5, 150, 24, 10, 48, False],  # 43.5 [A/s]
+    'BO-Fam:PS-SD':    [0.5, 150, 24, 10, 48, False],  # 43.5 [A/s]
+
+    'BO-02D:PS-QS':    [0.5, 10,  24, 10, 48, False],  # 2.9 [A/s]
+
+    'BO-01U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-03U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-05U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-07U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-09U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-11U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-13U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-15U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-17U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-19U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-21U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-23U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-25U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-27U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-29U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-31U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-33U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-35U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-37U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-39U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-41U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-43U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-45U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-47U:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-49D:PS-CH':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+
+    'BO-01U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-03U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-05U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-07U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-09U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-11U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-13U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-15U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-17U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-19U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-21U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-23U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-25U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-27U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-29U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-31U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-33U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-35U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-37U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-39U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-41U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-43U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-45U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-47U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+    'BO-49U:PS-CV':    [0.5, 10, 24, 10, 48, False],  # 2.9 [A/s]
+
 }
 
 
-def gen_waveform(dt, ampl, nrpts_period, nr_periods, tau_period, square):
+def gen_waveform(dt, ampl, period, nr_periods, tau, square):
     """."""
-    period = dt * nrpts_period
     w = 2*np.pi/period
-    tau = tau_period*period
-    nrpts = nrpts_period * nr_periods
+    nrpts = nr_periods * int(period / dt)
     v = list(range(0, nrpts))
     t = dt * np.array(v)
     n = 2 if square else 1
-    t0 = np.arctan(2*np.pi*tau_period*n)/w
+    t0 = np.arctan(2*np.pi*tau*n)/w
     sin = np.sin(2*np.pi*t/period)
     exp = np.exp(-(t-t0)/tau)
     amp = ampl/(np.sin(w*t0))**n
@@ -278,13 +285,18 @@ def ps_list(psname):
     dt = np.diff(t)
     dw = np.diff(w)
     print('{:<20s}: {:.02f} minutes, max {:.1f} A/s'.format(
-        psname, max(t)/60.0, max(dw/dt)))
+        psname, max(t)/60.0, max(abs(dw/dt))))
+
+    # plt.plot(dw/dt)
+    # plt.show()
 
 
 def ps_plot(psname):
     """."""
+    # t, w = gen_waveform(*parms[psname])
     t, w = gen_waveform(*parms[psname])
-    plt.plot(t, w, 'o')
+    plt.plot(t, w, '-b')
+    plt.plot(t, w, '.b')
     plt.xlabel('time [s]')
     plt.ylabel('Current [A]')
     plt.title(psname)
@@ -317,6 +329,7 @@ def ps_rampdown(psname):
 
 def ps_cycle(psname):
     """."""
+    # t, w = gen_waveform(*parms[psname])
     t, w = gen_waveform(*parms[psname])
     ps_rampdown(psname)
     pv_sp = create_pv_sp(psname)
