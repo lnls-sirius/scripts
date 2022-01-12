5b77021663515625f9507da4e3447a546ec7ec1a
ximenes <xresende@gmail.com>
2022-01-12 12:58:25 -0300

    Change backup script to save unziped tar file of screens-iocs

--- backups/sirius-script-backup-screens-iocs.bash
+++ backups/sirius-script-backup-screens-iocs.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 timestamp=`date '+%Y-%m-%d_%Hh%Mm%Ss'`
-cd /home/sirius/shared && tar zcfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tgz"
+# cd /home/sirius/shared && tar zcfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tgz"
+cd /home/sirius/shared && tar cfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tar"
