f12d8ef64571a526204353bbe00ee33de847666b
alex <aalexnc@gmail.com>
2022-10-23 04:02:48 -0300

    Completadas as listas CONTROLLERS e ION_PUMPS

--- bin/sirius-script-reset-ipumps.py
+++ bin/sirius-script-reset-ipumps.py
@@ -21,6 +21,113 @@ CONTROLLERS = {
          'SI-03BCFE:VA-SIP150-MD',
          'SI-RA03:VA-SIPC-05:C4',
         ], 
+
+    'SI-RA05:VA-SIPC-05':
+        ['SI-05SAFE:VA-SIP150-MD',
+         'SI-05C2FE:VA-SIP150-MD',
+         'SI-05BCFE:VA-SIP150-MD',
+         'SI-RA05:VA-SIPC-05:C4',
+        ],
+
+    'SI-RA06:VA-SIPC-04':
+        ['SI-06C4:VA-SIP20-BG',
+         'SI-07M1:VA-SIP20-BG',
+         'SI-06SBFE:VA-SIP150-MD',
+         'SI-06BCFE:VA-SIP150-MD',
+        ],
+
+    'SI-RA07:VA-SIPC-05':
+        ['SI-07SPFE:VA-SIP150-MD',
+         'SI-07C2FE:VA-SIP150-MD',
+         'SI-07BCFE:VA-SIP150-MD',
+         'SR-RA07:VA-SIPC-05:C4',
+        ],
+
+    'SI-RA09:VA-SIPC-05':
+        ['SI-09SAFE:VA-SIP150-MD',
+         'SI-09C2FE:VA-SIP150-MD',
+         'SI-09BCFE:VA-SIP150-MD',
+         'SR-RA09:VA-SIPC-05:C4',
+        ],
+
+    'SI-RA10:VA-SIPC-04':
+        ['SI-10C4:VA-SIP20-BG',
+         'SI-11M1:VA-SIP20-BG',
+         'SI-10SBFE:VA-SIP150-MD',
+         'SI-10BCFE:VA-SIP150-MD',
+        ],
+
+    'SI-RA11:VA-SIPC-05':
+        ['SI-11SPFE:VA-SIP150-MD',
+         'SI-11C2FE:VA-SIP150-MD',
+         'SI-11BCFE:VA-SIP150-MD',
+         'SR-RA11:VA-SIPC-05:C4',
+        ],
+
+    'SI-RA12:VA-SIPC-04':
+        ['SI-12C4:VA-SIP20-BG',
+         'SI-13M1:VA-SIP20-BG',
+         'SI-12SBFE:VA-SIP150-MD',
+         'SI-12BCFE:VA-SIP150-MD',
+        ],
+
+    'SI-RA13:VA-SIPC-05':
+        ['SI-13SAFE:VA-SIP150-MD',
+         'SI-13C2FE:VA-SIP150-MD',
+         'SI-13BCFE:VA-SIP150-MD',
+         'SR-RA13:VA-SIPC-05:C4',
+        ],
+
+    'SI-RA14:VA-SIPC-04':
+        ['SI-14C4:VA-SIP20-BG',
+         'SI-15M1:VA-SIP20-BG',
+         'SI-14SBFE:VA-SIP150-MD',
+         'SI-14BCFE:VA-SIP150-MD',
+        ],
+
+    'SI-RA15:VA-SIPC-05':
+        ['SI-15SPFE:VA-SIP150-MD',
+         'SI-15C2FE:VA-SIP150-MD',
+         'SI-15BCFE:VA-SIP150-MD',
+         'SR-RA15:VA-SIPC-05:C4',
+        ],
+
+    'SI-RA16:VA-SIPC-04':
+        ['SI-16C4:VA-SIP20-BG',
+         'SI-17M1:VA-SIP20-BG',
+         'SI-16SBFE:VA-SIP150-MD',
+         'SI-16BCFE:VA-SIP150-MD',
+        ],
+
+    'SI-RA17:VA-SIPC-05':
+        ['SI-17SAFE:VA-SIP150-MD',
+         'SI-17C2FE:VA-SIP150-MD',
+         'SI-17BCFE:VA-SIP150-MD',
+         'SR-RA17:VA-SIPC-05:C4',
+        ],
+
+    'SI-RA18:VA-SIPC-04':
+        ['SI-18C4:VA-SIP20-BG',
+         'SI-19M1:VA-SIP20-BG',
+         'SI-18SBFE:VA-SIP150-MD',
+         'SI-18BCFE:VA-SIP150-MD',
+        ],  
+
+    'SI-RA19:VA-SIPC-05':
+        ['SI-19SPFE:VA-SIP150-MD',
+         'SI-19C2FE:VA-SIP150-MD',
+         'SI-19BCFE:VA-SIP150-MD',
+         'SR-RA19:VA-SIPC-05:C4',
+        ],
+
+    'SI-RA20:VA-SIPC-06':
+        ['SI-20C4:VA-SIP20-BG',
+         'SI-01M1:VA-SIP20-BG',
+         'SI-20SBFE:VA-SIP150-MD',
+         'SI-20BCFE:VA-SIP150-MD',
+        ],
+
+
 }
 
 
@@ -45,6 +152,36 @@ ION_PUMPS = [
     'SI-18BCFE:VA-SIP150-MD',
     'SI-19BCFE:VA-SIP150-MD',
     'SI-20BCFE:VA-SIP150-MD',
+    'SI-01C2FE:VA-SIP150-MD',
+    'SI-03C2FE:VA-SIP150-MD',
+    'SI-05C2FE:VA-SIP150-MD',
+    'SI-07C2FE:VA-SIP150-MD',
+    'SI-09C2FE:VA-SIP150-MD',
+    'SI-11C2FE:VA-SIP150-MD',
+    'SI-13C2FE:VA-SIP150-MD',
+    'SI-15C2FE:VA-SIP150-MD',
+    'SI-17C2FE:VA-SIP150-MD',
+    'SI-19C2FE:VA-SIP150-MD',
+    'SI-02SBFE:VA-SIP150-MD',
+    'SI-04SBFE:VA-SIP150-MD',
+    'SI-06SBFE:VA-SIP150-MD',
+    'SI-08SBFE:VA-SIP150-MD',
+    'SI-10SBFE:VA-SIP150-MD',
+    'SI-12SBFE:VA-SIP150-MD',
+    'SI-14SBFE:VA-SIP150-MD',
+    'SI-16SBFE:VA-SIP150-MD',
+    'SI-18SBFE:VA-SIP150-MD',
+    'SI-20SBFE:VA-SIP150-MD',
+    'SI-01SAFE:VA-SIP150-MD',
+    'SI-05SAFE:VA-SIP150-MD',
+    'SI-09SAFE:VA-SIP150-MD',
+    'SI-13SAFE:VA-SIP150-MD',
+    'SI-17SAFE:VA-SIP150-MD',
+    'SI-03SPFE:VA-SIP150-MD',
+    'SI-07SPFE:VA-SIP150-MD',
+    'SI-11SPFE:VA-SIP150-MD',
+    'SI-15SPFE:VA-SIP150-MD',
+    'SI-19SPFE:VA-SIP150-MD',
     ]
 
 
