b3604e65d410acd2d628b6ab6599d498419e7168
ximenes <xresende@gmail.com>
2023-09-04 09:48:24 -0300

    Update backup script

--- backups/sirius-script-backup-screens-iocs.bash
+++ backups/sirius-script-backup-screens-iocs.bash
@@ -2,4 +2,6 @@
 
 timestamp=`date '+%Y-%m-%d_%Hh%Mm%Ss'`
 # cd /home/sirius/shared && tar zcfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tgz"
-cd /home/sirius/shared && tar cfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tar"
+# cd /home/sirius/shared && tar cfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tar"
+# cd /home/sirius/shared && rsync -avzh screens-iocs sirius@10.128.255.3:/storage/services/AutoBackup/FAC/$timestamp"
+cd /home/ximenes/repos-dev && rsync -avzh scripts -e ssh sirius@10.128.255.3:/storage/services/AutoBackup/FAC/$timestamp
