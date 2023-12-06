a098968382c1f8eed8b10491aeba1828e2029249
ximenes <xresende@gmail.com>
2023-12-06 16:22:44 -0300

    Update screens-iocs backup script to save backup in IBIRA

--- backups/sirius-script-backup-screens-iocs.bash
+++ backups/sirius-script-backup-screens-iocs.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 timestamp=`date '+%Y-%m-%d_%Hh%Mm%Ss'`
-# cd /home/sirius/shared && tar zcfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tgz"
-# cd /home/sirius/shared && tar cfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tar"
-# cd /home/sirius/shared && rsync -avzh screens-iocs sirius@10.128.255.3:/storage/services/AutoBackup/FAC/$timestamp"
-cd /home/sirius/repos-dev && rsync -avzh scripts -e ssh sirius@10.128.255.3:/storage/services/AutoBackup/FAC/$timestamp
+#cd /home/sirius/shared && tar cfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tar"
+#cd /home/sirius/shared && tar cfv - screens-iocs | ssh sirius@10.0.38.42 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tar"
+cd /home/sirius/shared && tar cfv - screens-iocs | ssh ximenes.resende@swc-server-ibm1 "cat > /mnt/nfs/ibira/lnls/labs/fac/data_by_day/screens-iocs_$timestamp.tar"
