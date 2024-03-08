62f3320811f150707917d165b3dcca98fc48c1dc
ximenes <xresende@gmail.com>
2024-03-07 21:29:13 -0300

    Update check-setpoints

--- bin/sirius-script-check-setpoints.py
+++ bin/sirius-script-check-setpoints.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python-sirius
 
 import time
+import numpy as np
 
 from siriuspy.clientarch import ClientArchiver, PVDataSet, Time
 
@@ -34,7 +35,7 @@ def create_connector(timeout=1):
         time.sleep(0.1)
     print('could not connect to configdb!')
     return None
-    
+
 
 def get_all_pvnames(conn):
     """."""
@@ -58,47 +59,70 @@ def categorize_pvnames(all_pvnames):
     return pvnames_sp, pvnames_rb, pvnames_others
 
 
-def check_pvs_set(conn, pvnames, time_start, time_stop):
+def check_pvs_set(conn, pvnames, time_start, time_stop, nrpvs, offset):
     """."""
-    # get data
-    pvds = PVDataSet(pvnames, connector=conn)
-    pvds.timeout = 30
-    pvds.time_start = time_start
-    pvds.time_stop = time_stop
-    pvds.update()
+    # get dataset 1
+    pvds1 = PVDataSet(pvnames, connector=conn)
+    pvds1.timeout = 30
+    pvds1.time_start = time_start
+    pvds1.time_stop = time_start + 60
+    pvds1.update()
+
+    # get dataset 2
+    pvds2 = PVDataSet(pvnames, connector=conn)
+    pvds2.timeout = 30
+    pvds2.time_start = time_stop - 60
+    pvds2.time_stop = time_stop
+    pvds2.update()
 
     # check
     for pvname in pvnames:
-        pvd = pvds[pvname]
-        stamp, value = pvd.timestamp, pvd.value
-        if stamp is not None and len(stamp) < 2:
+        # get pv data from the two time intervals and concatenate data
+        pvd1, pvd2 = pvds1[pvname], pvds2[pvname]
+        stamp = np.array(list(pvd1.timestamp) + list(pvd2.timestamp))
+        value = np.array(list(pvd1.value) + list(pvd2.value))
+
+        # sort concatenated data by timestamp
+        indices = np.argsort(stamp)
+        stamp = stamp[indices]
+        value = value[indices]
+
+        # remove duplicate data (same timestamp)
+        stamp, indices = np.unique(stamp, return_index=True)
+        value = value[indices]
+
+        offset += 1
+        # if only one data point, no change, continue loop...
+        if len(stamp) < 2:
+            # print('no change!')
             continue
-        try:
-            stamp0 = Time(stamp[0]).get_iso8601()
-            stamp1 = Time(stamp[-1]).get_iso8601()
-            print(f'{pvname} {len(stamp)} {stamp0} {stamp1} {len(set(value))}')
-        except TypeError:
-            print(f'{pvname} ...type error...')
-        except IndexError:
-            print(f'{pvname} ...index error...')
+        else:
+            print(f'{offset:06d}/{nrpvs:06d} - {pvname} - ', end='', flush=True)
+            print(f'{len(stamp):04d}')
+
+    return offset
 
 
+# parameters
+filter = 'BO*PS*'
+time_start = Time(2024,2,5,8,0,0)
+time_stop = Time(2024,2,6,8,1,0)
+nrchk = 50
+
+# list PV in categories
 conn = create_connector()
-all_pvnames = conn.getAllPVs('*PS*')
+all_pvnames = conn.getAllPVs(filter)
+print(f'filter        : {filter}')
 print(f'number of pvs : {len(all_pvnames)}')
 pvnames_sp, pvnames_rb, pvnames_others = categorize_pvnames(all_pvnames)
 print(f'nr sp pvs     : {len(pvnames_sp)}')
 print(f'nr rb pvs     : {len(pvnames_rb)}')
 print(f'nr other pvs  : {len(pvnames_others)}')
-time_start = Time(2024,2,5,8,0,0)
-time_stop = Time(2024,2,6,8,0,0)
 
-for pvname in pvnames_sp:
-    check_pvs_set(conn, [pvname], time_start, time_stop)
-
-# for i in range(50):
-#     print(i)
-#     i1 = min(len(pvnames_sp), 100*i)
-#     i2 = min(len(pvnames_sp), 100*(i+1))
-#     check_pvs_set(conn, pvnames_sp[i1:i2], time_start, time_stop)
-
+# run analysis
+offset = 0
+nrpvs = len(pvnames_sp)
+for i in range(1 + nrpvs // nrchk):
+    i1, i2 = min(nrchk*i, nrpvs), min(nrchk*(i+1), nrpvs)
+    pvnames = pvnames_sp[i1:i2]
+    offset = check_pvs_set(conn, pvnames, time_start, time_stop, nrpvs, offset)
