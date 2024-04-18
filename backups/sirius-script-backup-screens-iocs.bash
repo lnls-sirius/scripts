20b3b4cac0c6adfcd6455eeee25e8c337557cf90
Ximenes Rocha Resende <ximenes.resende@s-fac02-l.abtlus.org.br>
2024-04-18 14:32:25 -0300

    Update backup folder in ibira

--- backups/sirius-script-backup-screens-iocs.bash
+++ backups/sirius-script-backup-screens-iocs.bash
@@ -3,4 +3,5 @@
 timestamp=`date '+%Y-%m-%d_%Hh%Mm%Ss'`
 #cd /home/sirius/shared && tar cfv - screens-iocs | ssh sirius@10.128.255.3 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tar"
 #cd /home/sirius/shared && tar cfv - screens-iocs | ssh sirius@10.0.38.42 "cat > /storage/services/AutoBackup/FAC/screens-iocs_$timestamp.tar"
-cd /home/sirius/shared && tar cfv - screens-iocs | ssh ximenes.resende@swc-server-ibm1 "cat > /mnt/nfs/ibira/lnls/labs/fac/data_by_day/screens-iocs_$timestamp.tar"
+# cd /home/sirius/shared && tar cfv - screens-iocs | ssh ximenes.resende@swc-server-ibm1 "cat > /mnt/nfs/ibira/lnls/labs/fac/data_by_day/screens-iocs_$timestamp.tar"
+cd /home/sirius/shared && tar cfv - screens-iocs | ssh ximenes.resende@swc-server-ibm1 "cat > /mnt/nfs/ibira/lnls/labs/fac/backups/data_by_day/screens-iocs_$timestamp.tar"
