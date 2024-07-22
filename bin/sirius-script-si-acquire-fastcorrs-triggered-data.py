36febdd122dbb89ec2549a8a11fd456d25494e42
ximenes <xresende@gmail.com>
2024-07-22 12:26:26 -0300

    Fixes and improvements in fast corrector acquisition snippet

--- bin/sirius-script-si-acquire-fastcorrs-triggered-data.py
+++ bin/sirius-script-si-acquire-fastcorrs-triggered-data.py
@@ -21,16 +21,16 @@ def create_devices():
     return sofb, fofb, bbbl, bbbh, bbbv, bopsrmp, borfrmp
 
 
-def read_feedback_status(devs, lampacq):
+def read_feedback_status(devs, data):
     """."""
     sofb, fofb, bbbl, bbbh, bbbv, bopsrmp, borfrmp = devs
-    lampacq.data['sofb_loop_state'] = sofb.autocorrsts
-    lampacq.data['fofb_loop_state'] = fofb.loop_state
-    lampacq.data['bbbl_loop_state'] = bbbl.feedback.loop_state
-    lampacq.data['bbbh_loop_state'] = bbbh.feedback.loop_state
-    lampacq.data['bbbv_loop_state'] = bbbv.feedback.loop_state
-    lampacq.data['bo_ps_ramp_state'] = bopsrmp.is_on
-    lampacq.data['bo_rf_ramp_state'] = borfrmp.is_on
+    data['sofb_loop_state'] = sofb.autocorrsts
+    data['fofb_loop_state'] = fofb.loop_state
+    data['bbbl_loop_state'] = bbbl.feedback.loop_state
+    data['bbbh_loop_state'] = bbbh.feedback.loop_state
+    data['bbbv_loop_state'] = bbbv.feedback.loop_state
+    data['bo_ps_ramp_state'] = bopsrmp.is_on
+    data['bo_rf_ramp_state'] = borfrmp.is_on
 
 
 if __name__ == "__main__":
@@ -44,17 +44,18 @@ if __name__ == "__main__":
         help='name of the file to save (Default: FOFBLAMP-_YY-MM-DD_HHhMMmSSs)')
     # 3 minutes between top-up injections, timeout > 3*60s
     parser.add_argument(
-        '-b', '--nrptsbefore', type=int, default=1_000,
-        help='nr points before trigger (Default: 1000)')
+        '-b', '--nrptsbefore', type=int, default=500_000,
+        help='nr points before trigger (Default: 500000)')
     parser.add_argument(
-        '-a', '--nrptsafter', type=int, default=10_000,
-        help='nr points after trigger (Default: 10000)')
+        '-a', '--nrptsafter', type=int, default=500_000,
+        help='nr points after trigger (Default: 500000)')
 
     args = parser.parse_args()
 
     # prepare acquisition
-    lampacq = FamFOFBLamp()
+    lampacq = FamFOFBLamp(event='Study')
 
+    print('Configuring lamp...')
     for dev in lampacq.ctrldevs.values():
         dev.cmd_ctrl(1)  # stop
     for dev in lampacq.ctrldevs.values():
@@ -67,28 +68,37 @@ if __name__ == "__main__":
         dev.trigger = 1  # external
     for dev in lampacq.ctrldevs.values():
         dev.cmd_ctrl(0)  # start
-
-    _time.sleep(1)
+    print('...done...')
+    _time.sleep(2)
 
     # trigger acquisition
+    print('...sending trigger...')
     lampacq.evtdev.cmd_external_trigger()
+
+    # sleep waiting for data
+    print('...waiting for data...')
+    _time.sleep(30)
+    print('...done...')
 
     # create devices to complement data
     devs = create_devices()
 
     # get lamp data
+    print('...getting data...')
     data = dict()
     for datatyp in ['Current', 'Voltage']:
         data[datatyp] = dict()
         for psname, psdev in lampacq.psdevs.items():
-            data[datatyp][psname] = psdev[f'LAMP{datatyp}Data']
+            pvo = psdev.pv_object(f'LAMP{datatyp}Data')
+            data[datatyp][psname] = pvo.get(use_monitor=False, timeout=5.0)
 
     # get extra data
-    read_feedback_status(devs, lampacq)
+    read_feedback_status(devs, data)
 
     # save file
+    print('...saving data to file...')
     now = datetime.now()
     str_now = now.strftime('%Y-%m-%d_%Hh%Mm%Ss')
     filename = args.filename or 'FOFBLAMP' + str_now
-    save(data, filename + '.hdf5', overwrite=False)
+    save(data, filename + '.hdf5', overwrite=False, compress=True)
     print(f'\nData saved at {str_now:s}')
