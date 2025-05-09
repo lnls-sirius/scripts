a41fcbe77e97a7daf42b322e4f8318d847c0cfff
Matheus <vellosok75@gmail.com>
2025-05-09 15:47:10 -0300

    murilo's improvements
    Co-authored-by: murilobalves <alvesb.murilo@gmail.com>

--- bin/sirius-script-si-set-loco-strengths.py
+++ bin/sirius-script-si-set-loco-strengths.py
@@ -6,19 +6,71 @@ import sys
 
 import numpy as np
 from mathphys.functions import load_pickle
-from apsuite.commisslib.apply_strengths import SetOpticsMode, \
-    SISetTrimStrengths
+from apsuite.commisslib.apply_strengths import (
+    SetOpticsMode,
+    SISetTrimStrengths,
+)
+
+# Achromatic skew quadrupole variations to adjust betatron coupling,
+# it is -(v1 + v4)/np.sqrt(2) where v1 and v4 are the first and fourth
+# left-singular vectors of jacobian matrix calculated with
+# apsuite.optics.coupling_correction.calc_jacobian_matrix() using the
+# nominal SIRIUS lattice
+ACHROM_QS_ADJ = 1e-4 * np.array(
+    [
+        -0.53435125,
+        1.83818311,
+        1.98691151,
+        1.75361165,
+        1.56175837,
+        -0.88710175,
+        -1.15014152,
+        -2.18259391,
+        -0.53435125,
+        1.83818311,
+        1.98691151,
+        1.75361165,
+        1.56175837,
+        -0.88710175,
+        -1.15014152,
+        -2.18259391,
+        -0.53435125,
+        1.83818311,
+        1.98691151,
+        1.75361165,
+        1.56175837,
+        -0.88710175,
+        -1.15014152,
+        -2.18259391,
+        -0.53435125,
+        1.83818311,
+        1.98691151,
+        1.75361165,
+        1.56175837,
+        -0.88710175,
+        -1.15014152,
+        -2.18259391,
+        -0.53435125,
+        1.83818311,
+        1.98691151,
+        1.75361165,
+        1.56175837,
+        -0.88710175,
+        -1.15014152,
+        -2.18259391,
+    ]
+)
 
 
 def ask_yes_no(prompt):
     """Prompt user for yes/no input. Default to yes."""
-    print(f"{prompt} [Y/N]: ", end='', flush=True)
+    print(f"{prompt} [Y/N]: ", end="", flush=True)
     try:
         response = input().strip().lower()
     except KeyboardInterrupt:
         print("\nAborted.")
         sys.exit(1)
-    return response in ('', 'y', 'yes')
+    return response in ("", "y", "yes")
 
 
 def prompt_apply_then_check(apply_func):
@@ -37,7 +89,7 @@ def apply_family_average(folder):
     mag_sel = "quadrupoles"
     set_optics = SetOpticsMode(acc="SI", optics_mode=None)
     mags, init_strengths = set_optics.get_strengths(magname_filter=mag_sel)
-    fam_avg = load_pickle(folder + 'quad_family_average.pickle')
+    fam_avg = load_pickle(folder + "quad_family_average.pickle")
     delta_avg = np.array([fam_avg[mag.dev] for mag in mags])
     goal_stren = init_strengths - delta_avg
 
@@ -47,7 +99,7 @@ def apply_family_average(folder):
             magname_filter=mag_sel,
             percentage=100,
             apply=apply,
-            print_change=True
+            print_change=True,
         )
 
     _apply_strengths(apply=False)
@@ -60,8 +112,8 @@ def apply_normal_quad_trims(folder):
     print("Applying normal quads trims...")
     mag_sel = "quadrupoles"
     set_trims = SISetTrimStrengths(model=None)
-    mags, init_strengths = set_trims.get_strengths(magname_filter=mag_sel)
-    deltas = np.loadtxt(folder + 'quad_trims_deltakl_no_average__.txt')
+    _, init_strengths = set_trims.get_strengths(magname_filter=mag_sel)
+    deltas = np.loadtxt(folder + "quad_trims_deltakl_no_average__.txt")
     goal_stren = init_strengths - deltas
     mean = np.mean(deltas)
     print(f"Mean close to zero? {np.isclose(mean, 0)}. Mean: {mean}")
@@ -72,7 +124,7 @@ def apply_normal_quad_trims(folder):
             magname_filter=mag_sel,
             percentage=100,
             apply=apply,
-            print_change=True
+            print_change=True,
         )
 
     _apply_strengths(apply=False)
@@ -85,8 +137,8 @@ def apply_skew_quad_trims(folder):
     print("Applying skew quads trims...")
     mag_sel = "skew_quadrupole"
     set_trims = SISetTrimStrengths(model=None)
-    mags, init_strengths = set_trims.get_strengths(magname_filter=mag_sel)
-    deltas = np.loadtxt(folder + 'skewquad_deltaksl__.txt')
+    _, init_strengths = set_trims.get_strengths(magname_filter=mag_sel)
+    deltas = np.loadtxt(folder + "skewquad_deltaksl__.txt")
     goal_stren = init_strengths - deltas
 
     def _apply_strengths(apply=True):
@@ -95,7 +147,7 @@ def apply_skew_quad_trims(folder):
             magname_filter=mag_sel,
             percentage=100,
             apply=apply,
-            print_change=True
+            print_change=True,
         )
 
     _apply_strengths(apply=False)
@@ -103,14 +155,22 @@ def apply_skew_quad_trims(folder):
     prompt_apply_then_check(_apply_strengths)
 
 
-def correct_coupling():
-    """Correct coupling to 1%."""
-    print("Correcting coupling to 1%...")
-    mag_sel = '.*M1:PS-QS.*|.*M2:PS-QS.*'
+def control_coupling():
+    """Control coupling variation [%]."""
+    print("Insert desired coupling variation in [%]", end="", flush=True)
+    try:
+        coupling = float(input())
+    except KeyboardInterrupt:
+        print("\nAborted.")
+        sys.exit(1)
+    if not coupling:
+        return
+    print(f"Applying {coupling:.2f}% of coupling variation...")
+    mag_sel = ".*M1:PS-QS.*|.*M2:PS-QS.*"
     set_trims = SISetTrimStrengths(model=None)
-    mags, init_strengths = set_trims.get_strengths(magname_filter=mag_sel)
-    vhmat = np.loadtxt("../../machstudy_data/vh_from_coup_matrix.txt")
-    delta_stren = -(vhmat[0] + vhmat[3]) / np.sqrt(2) * 1e-3 * (+1.2)
+    _, init_strengths = set_trims.get_strengths(magname_filter=mag_sel)
+    # 1.2 is a heuristic factor to get a 1:1 relation to coupling variation
+    delta_stren = -ACHROM_QS_ADJ * 1.2 * coupling
     goal_stren = init_strengths + delta_stren
 
     def _apply_strengths(apply=True):
@@ -119,7 +179,7 @@ def correct_coupling():
             magname_filter=mag_sel,
             percentage=100,
             apply=apply,
-            print_change=True
+            print_change=True,
         )
 
     _apply_strengths(apply=False)
@@ -146,18 +206,18 @@ def print_help():
 
 def main():
     """."""
-    if '-h' in sys.argv or '--help' in sys.argv:
+    if "-h" in sys.argv or "--help" in sys.argv:
         print_help()
         sys.exit(0)
 
     folder = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
-    if not folder.endswith('/'):
-        folder += '/'
+    if not folder.endswith("/"):
+        folder += "/"
 
     apply_family_average(folder)
     apply_normal_quad_trims(folder)
     apply_skew_quad_trims(folder)
-    correct_coupling()
+    control_coupling()
 
 
 if __name__ == "__main__":
