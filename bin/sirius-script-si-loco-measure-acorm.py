717c91ec86bc7195954755d4fce005b9c50a938d
Matheus <vellosok75@gmail.com>
2026-04-06 14:55:15 -0300

    ENH (ACORM): modularize code
    - less of a monolith, more modular script
    - not yet tested

--- bin/sirius-script-si-loco-measure-acorm.py
+++ bin/sirius-script-si-loco-measure-acorm.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python-sirius
-"""Script for running & analyzing AC ORM measurements for LOCO fittings."""
+"""Run and analyze AC ORM measurements for LOCO."""
 
+import argparse
 import signal
 import sys
 import time
 from functools import partial
 from threading import Lock
 
-import numpy as _np
+import numpy as np
 from apsuite.commisslib.meas_ac_orm import ACORMParams, MeasACORM
 from mathphys.functions import load
 
-MEAS_TIMEOUT = 6 * 60  # [s]
-CONN_TIMEOUT = 15  # [s]
+MEAS_TIMEOUT_DEFAULT = 6 * 60  # [s]
+CONN_TIMEOUT_DEFAULT = 15  # [s]
 
 lock_stop = Lock()
 
@@ -39,97 +40,22 @@ def _stop_now(meas_orm, signum, frame):
     lock_stop.release()
 
 
-def configure(meas_orm, args):
+def parse_args():
     """."""
-    # only configuring parameters which differ from
-    # MeasACORM.params defaults or are changed by the flags of the script.
-    # TODO: review the class defaults. compare it to the last measrements.
-    meas_orm.params.ref_respmat_name = args.ref_respmat_name
-    meas_orm.params.correct_orbit_between_acqs = (
-        args.correct_orbit_between_acqs
-    )
-
-    meas_orm.params.corrs_norm_kicks = True
-    meas_orm.params.corrs_ch_kick = 5.000
-    meas_orm.params.corrs_cv_kick = 5.000
-    meas_orm.params.corrs_dorb1ch = 40.000
-    meas_orm.params.corrs_dorb1cv = 40.000
-
-    nrsecs = 1
-    primes = meas_orm.params.find_primes(2 * 8 * nrsecs + 2, 3)
-    # TODO: do we still need to exclude frequencies close to 60 Hz even
-    # now without the PetraVII Cavity?
-    primes = _np.array(sorted(set(primes) - {59, 61}))
-    cv_freqs = primes[: 8 * nrsecs]
-    primes = _np.array(sorted(set(primes) - set(cv_freqs)))
-    ch_freqs = primes[: 6 * nrsecs]
-
-    meas_orm.params.corrs_ch_freqs = ch_freqs
-    meas_orm.params.corrs_cv_freqs = cv_freqs
-
-    meas_orm.params.rf_mode = 'Standard'
-    meas_orm.params.rf_step_kick = 75 / 2
-    meas_orm.params.rf_step_delay = 0.2
-
-
-def check_configdb_entry(name, meas_orm, print_info=False):
-    """True if name is not a si_orbcorr_respm entry, False otherwise."""
-    try:
-        info = meas_orm.configdb.get_config_info(name)
-    except Exception as e:
-        return True
-    if print_info:
-        print(f'An ORM with name "{name}" already exists in configDB ')
-        print('Info:')
-        for key, val in info.items():
-            val = convert_timestamps(val)
-            print(f'\t{key}: {val}')
-    return False
-
-
-def convert_timestamps(val):
-    """."""
-    if isinstance(val, float):
-        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(val))
-
-    elif isinstance(val, list):
-        return [convert_timestamps(v) for v in val]
-    return val
-
-
-def check_previous_loco_input_data(name):
-    """."""
-    try:
-        ln = name + '_loco_input_data.pickle'
-        _ = load(ln)
-        print(
-            f'There already is a LOCO input data file named {ln}. '
-            ' Please, choose a different ORM name or delete/rename the '
-            'existing file. Exiting.'
-        )
-        return True
-    except FileNotFoundError:
-        return False
-
-
-def main():
-    """Parse params, configure and run the AC ORM measurement."""
-    import argparse as _argparse
-
     params = ACORMParams()
 
-    parser = _argparse.ArgumentParser(
-        description='Measure AC ORM for LOCO fitting.'
+    parser = argparse.ArgumentParser(
+        description='Measure AC ORM for LOCO fitting'
     )
 
     parser.add_argument(
         '-n',
         '--name',
         type=str,
-        default='acorm',
-        help='ORM nickname (without extension). Used for saving measurement '
+        default='acorm_' + time.strftime('%Y-%m-%d-%H-%M'),
+        help='ORM nickname (without extension) used for saving measurement '
         'acquisitions data, LOCO input data and for saving the ORM to the '
-        'configDB (if --save2configdb is set). Defaults to "acorm".',
+        'configDB. Defaults to "acorm_<year-month-day-hour-minute>".',
     )
 
     parser.add_argument(
@@ -149,17 +75,17 @@ def main():
     parser.add_argument(
         '--conn-timeout',
         type=int,
-        default=CONN_TIMEOUT,
+        default=CONN_TIMEOUT_DEFAULT,
         help='Time (in seconds) to wait for PVs to connect. Defaults to '
-        f'{CONN_TIMEOUT} seconds.',
+        f'{CONN_TIMEOUT_DEFAULT} seconds.',
     )
 
     parser.add_argument(
         '--meas-timeout',
         type=int,
-        default=MEAS_TIMEOUT,
+        default=MEAS_TIMEOUT_DEFAULT,
         help='Time (in seconds) to wait for measurement to finish. Defaults '
-        f'to {MEAS_TIMEOUT} seconds.',
+        f'to {MEAS_TIMEOUT_DEFAULT} seconds.',
     )
 
     parser.add_argument(
@@ -205,89 +131,171 @@ def main():
         f'Defaults to {params.correct_orbit_between_acqs}.',
     )
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    name = args.name
 
+def configure_measurement(meas_orm, args):
+    """."""
+    # only configuring parameters which differ from
+    # TODO: review the class defaults. compare it to the last measrements.
+    # MeasACORM.params defaults or are changed by the flags of the script.
+
+    p = meas_orm.params
+    p.ref_respmat_name = args.ref_respmat_name
+    p.correct_orbit_between_acqs = args.correct_orbit_between_acqs
+
+    p.corrs_norm_kicks = True
+    p.corrs_ch_kick = 5.0
+    p.corrs_cv_kick = 5.0
+    p.corrs_dorb1ch = 40.0
+    p.corrs_dorb1cv = 40.0
+
+    nrsecs = 1
+    primes = p.find_primes(2 * 8 * nrsecs + 2, 3)
+    # TODO: do we still need to exclude frequencies close to 60 Hz even
+    # now without the PetraVII Cavity?
+    primes = np.array(sorted(set(primes) - {59, 61}))
+    cv_freqs = primes[: 8 * nrsecs]
+    primes = np.array(sorted(set(primes) - set(cv_freqs)))
+    ch_freqs = primes[: 6 * nrsecs]
+
+    p.corrs_ch_freqs = ch_freqs
+    p.corrs_cv_freqs = cv_freqs
+
+    p.rf_mode = 'Standard'
+    p.rf_step_kick = 75 / 2
+    p.rf_step_delay = 0.2
+
+
+def config_exists(meas_orm, name, verbose=False):
+    """."""
+    try:
+        info = meas_orm.configdb.get_config_info(name)
+    except Exception as e:
+        print(f'ConfigDB error: {e}')
+        return False
+
+    if verbose:
+        print(f'An ORM w/ name "{name}" already exists in confgDB:')
+        for k, v in info.items():
+            print(f'\t{k}: {convert_timestamps(v)}')
+    return True
+
+
+def convert_timestamps(val):
+    """."""
+    if isinstance(val, float):
+        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(val))
+    if isinstance(val, list):
+        return [convert_timestamps(v) for v in val]
+    return val
+
+
+def loco_input_exists(name):
+    """."""
+    fname = f'{name}_loco_input_data.pickle'
+    try:
+        load(fname)
+        print(
+            f'A LOCO input data file named {fname} already exists. '
+            + 'Please, choose a different ORM name or delete/rename the '
+            'existing file.'
+        )
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def ensure_connection(meas_orm, timeout):
+    """."""
+    print('Connecting PVs...')
+    if meas_orm.wait_for_connection(timeout):
+        print('Connected.')
+        return True
+
+    print('Connection failed. Missing PVs:')
+    for pv in meas_orm.disconnected_pvnames:
+        print(f'\t{pv}')
+    return False
+
+
+def run_measurement(meas_orm, timeout):
+    """."""
+    meas_orm.start()
+
+    if not meas_orm.wait_measurement(timeout):
+        print('Measurement timeout.')
+        return False
+
+    return True
+
+
+def main():
+    """."""
+    args = parse_args()
     meas_orm = MeasACORM(isonline=True)
 
-    if check_configdb_entry(args.ref_respmat_name, meas_orm):
+    if not config_exists(meas_orm, args.ref_respmat_name):
+        print('Reference response matrix not found.')
         print(
-            'No reference respmat found with res name ' + args.ref_respmat_name
+            'Please, make sure the name passed to `--ref-respmat-name`'
+            + ' is valid.'
         )
-        print('Exiting.')
         sys.exit(1)
+
+    if loco_input_exists(args.name):
+        sys.exit(1)
+
+    configure_measurement(meas_orm, args)
 
     signal.signal(signal.SIGINT, partial(_stop_now, meas_orm))
     signal.signal(signal.SIGTERM, partial(_stop_now, meas_orm))
 
-    if check_previous_loco_input_data(name):
+    if not ensure_connection(meas_orm, args.conn_timeout):
         sys.exit(1)
-
-    configure(meas_orm, args)
-
-    print('Waiting PVs to connect...')
-
-    if not meas_orm.wait_for_connection(CONN_TIMEOUT):
-        print('\tSome PVs did not connect! Disconnected PVs:\n')
-        for pvname in meas_orm.disconnected_pvnames:
-            print(f'\t{pvname}')
-        print('Exiting.')
-        sys.exit(1)
-
-    print('\tDone!')
-
-    print('Measurement configured.')
 
     if args.print_setup:
         print(meas_orm.params)
 
     if not args.run_meas:
-        print(
-            'Exiting.'
-            + 'If you want to run the measurement, use the --run-meas flag.'
-        )
-        sys.exit(0)
+        print('Dry run. Use `--run-meas` to execute.')
+        return
 
-    print(80 * '#')
-    print('Starting AC ORM measurement.')
+    print('#' * 80)
+    print('Starting measurement...')
 
-    meas_orm.start()
-    meas_orm.wait_measurement(MEAS_TIMEOUT)
+    if not run_measurement(meas_orm, args.meas_timeout):
+        sys.exit(1)
+
     meas_orm.process_data()
 
-    print('Measurement finished and processed.')
-
-    print(f'\tFinished ok? {meas_orm.check_measurement_finished_ok()}')
-    print(f'\tGood quality? {meas_orm.check_measurement_quality()}')
+    print('Measurement finished & processed.')
+    print(f'\tFinished OK? {meas_orm.check_measurement_finished_ok()}')
+    print(f'\tQuality? {meas_orm.check_measurement_quality()}')
 
     if args.save_acq_data:
         print('Saving acquisitions data...')
-        meas_orm.save_data(name + '_acq_data.pickle')
-        print(
-            'Acquisitions data (unprocessed) saved to '
-            + f'{name}_acq_data.pickle'
-        )
+        fname = f'{args.name}_acq_data.pickle'
+        meas_orm.save_data(fname)
+        print(f'Saved: {fname}')
 
     print('Saving LOCO input data...')
-    meas_orm.save_loco_input_data(name + '_loco_input_data.pickle')
-    print(f'LOCO input data saved to {name}_loco_input_data.pickle')
+    loco_fname = f'{args.name}_loco_input_data.pickle'
+    meas_orm.save_loco_input_data(loco_fname)
+    print(f'Saved: {loco_fname}')
     print(
-        'Use `sirius-script-si-loco-run_fitting.py` to fit model to this data'
+        'Use `sirius-script-si-loco-run-fitting.py` to fit model to this data.'
     )
 
     if args.save2configdb:
-        if check_configdb_entry(name, meas_orm, True):
-            print('Saving measured AC ORM to configDB...')
-            meas_orm.save_respmat_to_configdb(name)
-            print('Done!')
+        if config_exists(meas_orm, args.name, verbose=True):
+            print('Aborting save.')
         else:
-            print('Data not saved to configDB!')
+            print('Saving measured AC ORM to configDB...')
+            meas_orm.save_respmat_to_configdb(args.name)
+            print('Saved.')
 
-    # meas_respmat = meas_orm.build_respmat()
-    # ref_respmat = meas_orm.get_ref_respmat()
-    # TODO: proceed with analysis and analysis report (similar to LOCO's)
-
+    # TODO: analysis & analysis report
 
 if __name__ == '__main__':
     main()
